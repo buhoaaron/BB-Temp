5ad5d4dcac1efde1d01c0106d577f8042c1705c2
szuwenchen <g2264664@hotmail.com>
2023-04-10 23:53:53 +0800

    New UIDebugObject
    非DEBUG_MODE時會自動關閉物件

---
+++ Assets/Scripts/Tools/UIDebugObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Barnabus.UI
+{
+    public class UIDebugObject : MonoBehaviour
+    {
+#if !DEBUG_MODE
+        private void Awake()
+        {
+            gameObject.SetActive(false);
+        }
+#endif
+    }
+}
