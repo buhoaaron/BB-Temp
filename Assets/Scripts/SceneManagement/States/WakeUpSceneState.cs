514b09df9b8aa60f0b58eee3ca7f9c483e12deca
szuwenchen <g2264664@hotmail.com>
2023-04-10 10:41:43 +0800

    Update six Barnabus spine
    同時重構播角色叫聲的功能

--- Assets/Scripts/SceneManagement/States/WakeUpSceneState.cs
+++ Assets/Scripts/SceneManagement/States/WakeUpSceneState.cs
@@ -10,7 +10,8 @@ namespace Barnabus.SceneManagement
     public class WakeUpSceneState : BaseSceneState
     {
         protected WakeUpUI wakeUpUI = null;
-
+        protected EventData soundEventData;
+        protected PlayerBarnabusData WakeUpBarnabusData;
         public WakeUpSceneState(SceneStateController controller, string sceneName) : base(controller, sceneName)
         {}
 
@@ -19,12 +20,12 @@ namespace Barnabus.SceneManagement
             wakeUpUI = GameObject.Find("Canvas_WakeUp").GetComponent<WakeUpUI>();
             wakeUpUI.Init();
 
-            var playerWakeUpBarnabusData = controller.GameManager.PlayerDataManager.UnlockBarnabusData;
+            WakeUpBarnabusData = controller.GameManager.PlayerDataManager.UnlockBarnabusData;
             //喚醒
-            playerWakeUpBarnabusData.IsWokenUp = true;
+            WakeUpBarnabusData.IsWokenUp = true;
             //更換玩家選擇的角色
-            wakeUpUI.ChangeBarnabusSpine(playerWakeUpBarnabusData.CharacterID);
-            wakeUpUI.SetBarnabusName(playerWakeUpBarnabusData.Name);
+            wakeUpUI.ChangeBarnabusSpine(WakeUpBarnabusData.CharacterID);
+            wakeUpUI.SetBarnabusName(WakeUpBarnabusData.Name);
 
             wakeUpUI.OnButtonReturnClick = BackMainAndOpenShelf;
             wakeUpUI.OnButtonLessonsClick = BackMainAndOpenLessons;
@@ -42,24 +43,20 @@ namespace Barnabus.SceneManagement
             DoWakeUp();
         }
 
-        //TOFIX: 待美術統一EventName後要重構
-        EventData eventData;
         protected void DoWakeUp()
         {
             wakeUpUI.SpineBarnabus.AnimationState.SetAnimation(0, SpineLabels.BarnabusAction2, false);
             wakeUpUI.SpineBarnabus.AnimationState.AddAnimation(0, SpineLabels.BarnabusIdle, true, 0);
 
-            eventData = wakeUpUI.SpineBarnabus.Skeleton.Data.FindEvent("s_anger");
+            soundEventData = wakeUpUI.SpineBarnabus.Skeleton.Data.FindEvent("sound");
             wakeUpUI.SpineBarnabus.AnimationState.Event += HandleAnimationStateEvent;
         }
 
         private void HandleAnimationStateEvent(TrackEntry trackEntry, Spine.Event e)
         {
-            bool eventMatch = (eventData == e.Data); 
+            bool eventMatch = (soundEventData == e.Data); 
             if (eventMatch)
-            {
-                controller.GameManager.AudioManager.PlaySound(AUDIO_NAME.ANGER_SOUND_01);
-            }
+                controller.GameManager.AudioManager.PlaySound(WakeUpBarnabusData.SoundKey);
         }
 
         private void BackMainAndOpenShelf()
