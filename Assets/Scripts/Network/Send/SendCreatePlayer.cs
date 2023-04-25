5ffb048bcfaefab79e0deaf64cb857825c2bde3e
SzuWenChen <g2264664@hotmail.com>
2023-04-25 13:51:18 +0800

    Update LoginScene
    1. 回復預設狀態
    2. 新增CreatePlayer資料類別

---
+++ Assets/Scripts/Network/Send/SendCreatePlayer.cs
@@ -0,0 +1,24 @@
+﻿namespace Barnabus.Network
+{
+    public class SendCreatePlayer : BaseSendPacket
+    {
+        public readonly int user_id;
+        public readonly bool is_parent_owned;
+        public readonly string first_name;
+        public readonly string last_name;
+        public readonly string grade;
+        public readonly string country_code;
+        public readonly string state;
+
+        public SendCreatePlayer(int user_id, bool is_parent_owned, string first_name, string last_name, string grade, string country_code, string state)
+        {
+            this.user_id = user_id;
+            this.is_parent_owned = is_parent_owned;
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.grade = grade;
+            this.country_code = country_code;
+            this.state = state;
+        }
+    }
+}
