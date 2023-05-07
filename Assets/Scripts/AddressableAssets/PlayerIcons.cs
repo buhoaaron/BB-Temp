3a76c2d6e6cadcf864752a93c397fd6008f009d6
SzuWenChen <g2264664@hotmail.com>
2023-05-08 01:12:35 +0800

    更新 PlayerIcons 貼圖

--- Assets/Scripts/AddressableAssets/PlayerIcons.cs
+++ Assets/Scripts/AddressableAssets/PlayerIcons.cs
@@ -5,7 +5,7 @@ namespace Barnabus
 {
     public class PlayerIcons : List<Sprite>
     {
-        private string iconFormat = "BG_PlayerID{0}_{1}";
+        private string iconFormat = "avatar_icon_{0}_{1}";
 
         public PlayerIcons(IEnumerable<Sprite> collection) : base(collection)
         {}
